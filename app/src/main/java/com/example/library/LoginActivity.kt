213f7409b6eec02f88c1b2bb4ca4fc0a7576cd10
NiniDolidze <84011073+NiniDolidze@users.noreply.github.com>
2022-01-22 10:37:43 +0400

    commit

--- app/src/main/java/com/example/library/LoginActivity.kt
+++ app/src/main/java/com/example/library/LoginActivity.kt
@@ -1,11 +1,55 @@
 package com.example.library
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.library.databinding.ActivityLoginBinding
+import com.example.library.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var progressDialog: ProgressDialog
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog.ProgressDialog(this)
+        progressDialog.setTitle("გთხოვთ მოიცადოთ")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.noAccountTv.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        binding.loginButton.setOnClickListener {
+            validateData()
+        }
     }
+
+    private val email = ""
+    private val password = ""
+
+    private fun validateData() {
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches() ){
+            Toast.makeText(this, "შეიყვანეთ სწორი ელ-ფოსტის მისამართი")
+        }
+
+
+    }
+
 }
