55a3a229df785c9e7ce4e7728809f2be694c4797
NiniDolidze <84011073+NiniDolidze@users.noreply.github.com>
2022-01-29 04:33:22 +0400

    commit

--- app/src/main/java/com/example/library/PdfAddActivity.kt
+++ app/src/main/java/com/example/library/PdfAddActivity.kt
@@ -1,11 +1,17 @@
 package com.example.library
 
+import android.app.Application
 import android.app.ProgressDialog
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Display
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.example.library.databinding.ActivityPdfAddBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -13,6 +19,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class PdfAddActivity : AppCompatActivity() {
 
@@ -43,6 +50,68 @@ class PdfAddActivity : AppCompatActivity() {
         binding.categoryTv.setOnClickListener { 
             categoryPickDialog()
         }
+
+        binding.attachPdfButton.setOnClickListener {
+            pdfPickIntent()
+        }
+
+        binding.submitButton.setOnClickListener {
+            vaidateData()
+        }
+    }
+
+    private var title = ""
+    private var description = ""
+    private var category = ""
+
+    private fun vaidateData() {
+        Log.d(TAG, "vaidateData: validating data")
+
+        title = binding.titleEt.text.toString().trim()
+        description = binding.descriptionEt.text.toString().trim()
+        category = binding.categoryTv.text.toString().trim()
+
+        if (title.isEmpty()){
+            Toast.makeText(this, "შეიყვანეთ სახელი...", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (description.isEmpty()){
+            Toast.makeText(this, "შეიყვანეთ აღწერა...", Toast.LENGTH_SHORT).show()
+        }
+        else if (category.isEmpty()){
+            Toast.makeText(this, "აირჩიეთ კატეგორია...", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (pdfUri == null){
+            Toast.makeText(this, "აირჩიეთ PDF ფაილი...", Toast.LENGTH_SHORT).show()
+        }
+
+        else {
+            uploadPdfToStorage()
+        }
+
+
+    }
+
+    private fun uploadPdfToStorage() {
+
+        Log.d(TAG, "uploadPdfToStorage: uploading to storage...")
+
+        progressDialog.setMessage("მიმდინარეობს ატვირთვა")
+        progressDialog.show()
+
+        val timestamp = System.currentTimeMillis()
+
+        val filePathAndName = "Books/$timestamp"
+
+        val storageReference = FirebaseStorage.getInstance().getReference(filePathAndName)
+        storageReference.putFile(pdfUri!!)
+            .addOnSuccessListener {
+
+            }
+            .addOnFailureListener {
+
+            }
     }
 
     private fun loadPdfCategories() {
@@ -94,6 +163,26 @@ class PdfAddActivity : AppCompatActivity() {
             .show()
     }
     private fun pdfPickIntent(){
-        Log.d(TAG, "pdfPickIntent: ")
+        Log.d(TAG, "pdfPickIntent: starting pdf pick intent")
+
+        val intent = Intent()
+        intent.type = "application/pdf"
+        intent.action = Intent.ACTION_GET_CONTENT
+        pdfActivityResultLauncher.launch(intent)
+
     }
+
+    val pdfActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult>{ result ->
+            if (result.resultCode == RESULT_OK){
+                Log.d(TAG, "PDF არჩეულია ")
+                pdfUri = result.data!!.data
+            }
+            else{
+                Log.d(TAG, "PDF არჩევა შეწყვეტილია ")
+                Toast.makeText(this, "პროცესი შეჩერებულია", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
 }
